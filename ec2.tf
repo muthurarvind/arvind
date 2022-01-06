33f2b1ca01b32e2ec73744afef85493028677f54
muthurarvind <94586791+muthurarvind@users.noreply.github.com>
2022-01-06 08:19:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_instance" "myec2" {
-  ami           = "ami099720109477/ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20211129-"
+  ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   subnet_id   = "subnet-049b97e1d7c38467c"
   key_name = "sshkey1"
@@ -11,6 +11,3 @@ resource "aws_instance" "myec2" {
     Name = "Ec2tf"
   }
 }
-© 2022 GitHub, Inc.
-Terms
-Pr
